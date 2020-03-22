1de153854b9cd943055b75a0d43a3972c1058663
Connor <clarson1229@live.com>
2020-03-22 13:31:56 -0500

    updated rest api

--- REST_api_files/login.php
+++ REST_api_files/login.php
@@ -1,23 +1,29 @@
 <?php
 
 $db = "on_me";
-$dbuser = "root";
-$pass = "gunni";
+$dbuser = "";
+$pass = "";
 $host = "localhost";
 $username = $_POST["user"];
 $password = $_POST["password"];
 
 $conn = mysqli_connect($host,$dbuser, $pass, $db);
 if ($conn){
-        $q = "SELECT * from Users_table Where id_User like '$username' and password_user like '$passw$
+        $q = "SELECT * from Users_table Where id_User like '$username' and pass$
         $result = mysqli_query($conn, $q);
         if (mysqli_num_rows($result) > 0){
-                echo "Login Sucess \n";
                 $row = mysqli_fetch_assoc($result);
-                echo $row["id_User"];
+                $response->status = true;
+                $response->userName = $row["id_User"];
+                $myObj->response= $response;
+                $myJSON = json_encode($myObj);
+                echo $myJSON;
          }else{
-                echo "UserName not found";
-                echo "Login failed!!!";
+                $response->status = false;
+                $response->userName = "NULL";
+                $myObj->response=$response;
+                $myJSON = json_encode($myObj);
+                echo $myJSON;
         }
 }else {
         echo "not connected";
