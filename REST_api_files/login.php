ac83917ad3886c0d398bb0dc3d89572c897925ed
Connor <clarson1229@live.com>
2020-03-29 21:08:08 -0500

    added to the api

--- REST_api_files/login.php
+++ REST_api_files/login.php
@@ -9,7 +9,7 @@ $password = $_POST["password"];
 
 $conn = mysqli_connect($host,$dbuser, $pass, $db);
 if ($conn){
-        $q = "SELECT * from Users_table Where id_User like '$username' and pass$
+        $q = "SELECT * from Users_table Where id_User like '$username' and password_user like '$password'";
         $result = mysqli_query($conn, $q);
         if (mysqli_num_rows($result) > 0){
                 $row = mysqli_fetch_assoc($result);
