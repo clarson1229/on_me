38d747c772a980a7d50f7bf5fbb42f45c193f461
Connor <clarson1229@live.com>
2020-03-29 21:22:03 -0500

    added the basic funcationality to favorites

--- app/src/main/java/com/connorlarson/onme/ui/favorites/FavoritesFragment.java
+++ app/src/main/java/com/connorlarson/onme/ui/favorites/FavoritesFragment.java
@@ -1,9 +1,12 @@
 package com.connorlarson.onme.ui.favorites;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,24 +15,229 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.connorlarson.onme.FavBarAdapter;
+import com.connorlarson.onme.FavDrinkAdapter;
+import com.connorlarson.onme.MainActivity;
 import com.connorlarson.onme.R;
+import com.connorlarson.onme.ui.profile.FavBar;
+import com.connorlarson.onme.ui.profile.FavDrink;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class FavoritesFragment extends Fragment {
 
-    private FavoritesViewModel favoritesViewModel;
+    private static final String TAG = "Favorites Page";
+    private View mView;
+    private ListView favBarsListView;
+    private ListView favDrinksListView;
+    // data vars
+    private MainActivity activity;
+    private String userId;
+    private ArrayList<FavBar> favBarArray =  new ArrayList<FavBar>();
+    private ArrayList<FavDrink> favDrinkArray =  new ArrayList<FavDrink>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        favoritesViewModel =
-                ViewModelProviders.of(this).get(FavoritesViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_favorites, container, false);
-        final TextView textView = root.findViewById(R.id.text_favorites);
-        favoritesViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+        mView = inflater.inflate(R.layout.fragment_favorites, container, false);
+        favBarsListView = mView.findViewById(R.id.favorite_bar_ListView);
+        favDrinksListView = mView.findViewById(R.id.favorite_drink_ListView);
+
+        activity = (MainActivity) getActivity();
+        userId = activity.getUserID();
+        updateScrollViews();
+
+        return mView;
+    }
+    private void updateScrollViews() {
+        FavoritesFragment.getFavBars GFB = new getFavBars();
+        GFB.execute(userId);
+
+        FavoritesFragment.getFavDrinks GFD = new getFavDrinks();
+        GFD.execute(userId);
+    }
+
+    private class getFavBars extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String result="";
+            String user = params[0];
+
+            String connstr = "http://connorlarson.ddns.net/restapi/fav_bars.php";
+
+            try{
+                URL url = new URL(connstr);
+                String param = "user="+user;
+                Log.d(TAG, "param:" + param);
+
+                // Open a connection using HttpURLConnection
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                con.setReadTimeout(7000);
+                con.setConnectTimeout(7000);
+                con.setDoOutput(true);
+                con.setDoInput(true);
+                con.setInstanceFollowRedirects(false);
+                con.setRequestMethod("POST");
+                con.setFixedLengthStreamingMode(param.getBytes().length);
+                con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                // Send
+                PrintWriter out = new PrintWriter(con.getOutputStream());
+                Log.d(TAG, "out Stream" + out);
+                out.print(param);
+                out.close();
+
+                con.connect();
+                BufferedReader in = null;
+                if (con.getResponseCode() != 200) {
+                    in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                    Log.d(TAG, "!=200: " + in);
+
+                } else {
+                    in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    Log.d(TAG, "POST request send successful: " + in);
+
+                    String line = null;
+                    StringBuilder sb = new StringBuilder();
+                    while((line = in.readLine()) != null)
+                    {
+                        // Append server response in string
+                        sb.append(line);
+                    }
+                    result = sb.toString();
+                    con.disconnect();
+                    return result;
+                }
+            }catch (MalformedURLException ex) {
+                ex.printStackTrace();
+            }catch (IOException ex) {
+                ex.printStackTrace();
             }
-        });
-        return root;
+            return result;
+        }
+        @Override
+        protected void onPostExecute(String result){
+            Log.d(TAG, "onPostExecute: Result = "+ result);
+            try {
+                JSONObject reader = new JSONObject(result);
+                JSONArray favBarsArray = reader.getJSONArray("FavBars");
+                favBarArray.clear();
+                for (int i = 0; i< favBarsArray.length(); i++){
+                    JSONObject obj = favBarsArray.getJSONObject(i);
+                    String tempFavId = obj.getString("FavBarId");
+                    String tempName = obj.getString("BarName");
+                    String tempAddress = obj.getString("BarAddress");
+                    String tempResId = obj.getString("RestaurantId");
+                    Log.d(TAG, "onPostExecute: id="+ tempFavId +" name=" + tempName + " address=" + tempAddress +" ResId=" +tempResId);
+
+                    FavBar favBar = new FavBar(tempName,tempAddress,tempFavId,tempResId);
+                    favBarArray.add(favBar);
+                }
+
+                Log.d(TAG, "onPostExecute: setting adapter");
+                FavBarAdapter favBarAdapter = new FavBarAdapter(activity, favBarArray);
+                favBarsListView.setAdapter(favBarAdapter);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+    private class getFavDrinks extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String result="";
+            String user = params[0];
+
+            String connstr = "http://connorlarson.ddns.net/restapi/fav_drinks.php";
+
+            try{
+                URL url = new URL(connstr);
+                String param = "user="+user;
+                Log.d(TAG, "param:" + param);
+
+                // Open a connection using HttpURLConnection
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                con.setReadTimeout(7000);
+                con.setConnectTimeout(7000);
+                con.setDoOutput(true);
+                con.setDoInput(true);
+                con.setInstanceFollowRedirects(false);
+                con.setRequestMethod("POST");
+                con.setFixedLengthStreamingMode(param.getBytes().length);
+                con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                // Send
+                PrintWriter out = new PrintWriter(con.getOutputStream());
+                Log.d(TAG, "out Stream" + out);
+                out.print(param);
+                out.close();
+
+                con.connect();
+                BufferedReader in = null;
+                if (con.getResponseCode() != 200) {
+                    in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                    Log.d(TAG, "!=200: " + in);
+
+                } else {
+                    in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    Log.d(TAG, "POST request send successful: " + in);
+
+                    String line = null;
+                    StringBuilder sb = new StringBuilder();
+                    while((line = in.readLine()) != null)
+                    {
+                        // Append server response in string
+                        sb.append(line);
+                    }
+                    result = sb.toString();
+                    con.disconnect();
+                    return result;
+                }
+            }catch (MalformedURLException ex) {
+                ex.printStackTrace();
+            }catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            return result;
+        }
+        @Override
+        protected void onPostExecute(String result){
+            Log.d(TAG, "onPostExecute: Result = "+ result);
+            try {
+                JSONObject reader = new JSONObject(result);
+                JSONArray favBarsArray = reader.getJSONArray("FavDrinks");
+                favDrinkArray.clear();
+                for (int i = 0; i< favBarsArray.length(); i++){
+                    JSONObject obj = favBarsArray.getJSONObject(i);
+                    String tempDrinkId = obj.getString("DrinkId");
+                    String tempName = obj.getString("DrinkName");
+                    String tempDescription = obj.getString("DrinkDescription");
+                    Log.d(TAG, "onPostExecute: id="+ tempDrinkId +" name=" + tempName + " description=" + tempDescription);
+
+                    FavDrink favDrink = new FavDrink(tempDrinkId,tempName,tempDescription);
+                    favDrinkArray.add(favDrink);
+                }
+                Log.d(TAG, "onPostExecute: setting adapter");
+                    FavDrinkAdapter favDrinkAdapter = new FavDrinkAdapter(activity, favDrinkArray);
+                favDrinksListView.setAdapter(favDrinkAdapter);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
